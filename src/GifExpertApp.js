5e0f246121bd9ec11d30382679ab9fc5edc1a7da
daftnemesis <XxdaftnemesisxX@gmail.com>
2022-03-25 21:44:21 -0600

    Aprendiendo a imprimir en pantalla varios objetos
    Usando el metodo map propia de los arrays, y tambien usando el hook useState para agregar nuevos elementos con un boton

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export const GifExpertApp = () => {
+  // const categories = ['One Punch', 'Samurai X', 'Dragon Ball'];
+  const [categories, setCategories] = useState(['One Punch', 'Samurai X', 'Dragon Ball']);
+
+  const handleAdd = () => {
+    //para agregar elementos a un estado, se usa el spread operator
+    // setCategories([...categories, 'Mazinger Z'])
+
+    //o con un callback
+    setCategories(cats => [...cats, 'Mazinger Z'])
+  }
+
   return (
     <>
       <h2>GifExpertApp</h2>
-      <hr/>
+      <hr />
+
+      <button onClick={handleAdd}>Agregar</button>
+
+      <ol>
+        {categories.map((category) => (
+          <li key={category}>{category}</li>
+        ))}
+      </ol>
     </>
-  )
-}
+  );
+};
