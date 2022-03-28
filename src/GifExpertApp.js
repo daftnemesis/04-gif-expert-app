d436b6c34a6981ca3a3fee28742e71201fee22b8
daftnemesis <XxdaftnemesisxX@gmail.com>
2022-03-27 22:38:22 -0600

    montando las targetas para representar los gifs

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -3,7 +3,7 @@ import { AddCategory } from './components/AddCategory';
 import { GifGrid } from './components/GifGrid';
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(['One Punch']);
+  const [categories, setCategories] = useState([]);
 
   // const handleAdd = () => {
   //   //para agregar elementos a un estado, se usa el spread operator
--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
+  const [images, setImages] = useState([])
+
+  // el hook useEffect se usa para hacer cambios en el estado pero solo cuando el componente es renderizado
+  useEffect( () => {
+    getGifs();
+  }, [])
+
   // Metodo para usar la API de giphy, usando un parametro predeterminado
   const getGifs = async () => {
-    const url = 'https://api.giphy.com/v1/gifs/search?q=dragon ball&limit=10&api_key=jnfYUZIxm7WR1pG7mWAcxrSQ7pGkJv5h'; // url de la peticion
+    const url = 'https://api.giphy.com/v1/gifs/search?q=scooby &limit=10&api_key=jnfYUZIxm7WR1pG7mWAcxrSQ7pGkJv5h'; // url de la peticion
     const resp = await fetch(url); //se usa el fetch
     const {data} = await resp.json(); // se pasa a formato json para poder trabajar con el
 
@@ -12,19 +20,31 @@ export const GifGrid = ({category}) => {
     const gifs = data.map(img => {
       return {
         id: img.id,
-        tittle: img.tittle,
+        title: img.title,
         url: img.images.downsized_medium.url,
       }
     })
 
     console.log(gifs);
+
+    setImages(gifs) // se llena el arreglo con los gifs retornados de la peticion
   }
 
-  getGifs();
-
   return (
-    <div>
+    <>
       <h3>{category}</h3>
-    </div>
+      <div className='card-grid'>
+        <ol>
+          {
+            images.map( img => (
+              <GifGridItem 
+                key={img.id}
+                {...img}
+              />
+            ))
+          }
+        </ol>
+      </div>
+    </>
   )
 }
