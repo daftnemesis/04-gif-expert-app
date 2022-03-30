e4d585fb4eb03fa3fe1bcaf96770fa36441ea7b0
daftnemesis <XxdaftnemesisxX@gmail.com>
2022-03-29 22:33:02 -0600

    creando un custom hook
    useFetchGifs, un custom hook para cargar el trabajo en un solo hook

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,25 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import { getGifs } from '../helpers/getGifs';
+import React from 'react'
+import { useFetchGifs } from '../hooks/useFetchGifs';
+//import { getGifs } from '../helpers/getGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({category}) => {
 
-  const [images, setImages] = useState([])
+  //const [images, setImages] = useState([])
 
   // el hook useEffect se usa para hacer cambios en el estado pero solo cuando el componente es renderizado
-  useEffect( () => {
-    getGifs(category)
-      .then(img => setImages(img));
-  }, [category])
+  // useEffect( () => {
+  //   getGifs(category)
+  //     .then(img => setImages(img));
+  // }, [category])
 
-  
+  const {data, loading} = useFetchGifs(category)
+
 
   return (
     <>
       <h3>{category}</h3>
+
+      {loading && <p>Loading</p>}
+
       <div className='card-grid'>
           {
-            images.map( img => (
+            data.map( img => (
               <GifGridItem 
                 key={img.id}
                 {...img}
